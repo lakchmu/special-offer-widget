d7da80fd3fd51bcd86ec921106d818f7222c531b
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-01-31 21:27:15 +0600

    Edit test for OfferModel class

--- src/__tests__/offerModel.test.js
+++ src/__tests__/offerModel.test.js
@@ -1,23 +1,16 @@
-jest.mock('../TBFSpecialOffers/request');
-
-import getOffers from '../TBFSpecialOffers/getOffers';
 import OfferModel from '../TBFSpecialOffers/offerModel';
 import { MOCK_GET_OFFERS } from '../TBFSpecialOffers/__mocks__/constants';
 
-test('class OfferModel', () => {
-  const token = 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0';
-  const filters = {};
-  getOffers(token, filters).then( response => {
-    let testOffer = JSON.parse(MOCK_GET_OFFERS).special_offers[0];
-    let offerModel = new OfferModel(testOffer);
-    expect(offerModel.title).toBe(testOffer.title);
-    expect(offerModel.shortDescription).toBe(testOffer.short_description);
-    expect(offerModel.description).toBe(testOffer.description);
-    expect(offerModel.dateFrom).toBe(testOffer.date_from);
-    expect(offerModel.dateTo).toBe(testOffer.date_to);
-    expect(offerModel.imageLink).toBe(testOffer.image);
-    expect(offerModel.discountValue).toBe(testOffer.discount_value);
-    expect(offerModel.discountType).toBe(testOffer.discount_type);
-    expect(offerModel.bookingLink).toBe(testOffer.link);
-  });
+test('Test OfferModel class', () => {
+  const testOffer = JSON.parse(MOCK_GET_OFFERS).special_offers[0];
+  const offerModel = new OfferModel(testOffer);
+  expect(typeof offerModel.title).toBe('string');
+  expect(typeof offerModel.shortDescription).toBe('string');
+  expect(typeof offerModel.description).toBe('string');
+  expect(typeof offerModel.dateFrom).toBe('string');
+  expect(typeof offerModel.dateTo).toBe('string');
+  expect(typeof offerModel.imageLink).toBe('string');
+  expect(typeof offerModel.discountValue).toBe('string');
+  expect(typeof offerModel.discountType).toBe('string');
+  expect(typeof offerModel.bookingLink).toBe('string');
 });
