f70a9f957fe52b47285692fa63777d0c6d53944c
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-01-31 21:20:47 +0600

    Edit test for getOffers function
    Add a test for the promise. Use ESLint.

--- src/__tests__/getOffers.test.js
+++ src/__tests__/getOffers.test.js
@@ -3,10 +3,15 @@ jest.mock('../TBFSpecialOffers/request');
 import getOffers from '../TBFSpecialOffers/getOffers';
 import OffersStorage from '../TBFSpecialOffers/offersStorage';
 
-it('works with promises and returns object of OffersStorage', () => {
+const token = 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0';
+const filters = {};
+
+it('Function getOffers returns object of OffersStorage', () => {
   expect.assertions(1);
-  const token = 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0';
-  const filters = {};
   return getOffers(token, filters)
     .then(offersStorage => expect(offersStorage).toBeInstanceOf(OffersStorage));
+});
+
+test('Function getOffers works with promise', () => {
+  expect(getOffers(token, filters)).toBeInstanceOf(Promise);
 });
