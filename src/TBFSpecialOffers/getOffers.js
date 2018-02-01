e1dcca6ea6aa693cc7d0f8be0a522dbdef21093d
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-01 17:46:29 +0600

    Add check that API answer contains errors

--- src/TBFSpecialOffers/getOffers.js
+++ src/TBFSpecialOffers/getOffers.js
@@ -6,7 +6,12 @@ function getOffers(token, filters) {
   const url = API_END_POINT + API_METHOD_SPECIAL_OFFERS;
   return request(url, 'get', token)
     .then(response => response.json())
-    .then(json => new OffersStorage(json.special_offers));
+    .then((json) => {
+      if (json.errors) {
+        throw new Error(json.errors.reduce((previousError, currentError) => `${previousError}, ${currentError}`));
+      }
+      return new OffersStorage(json.special_offers);
+    });
 }
 
 export default getOffers;
