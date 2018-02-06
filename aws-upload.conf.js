8719351d8383d89cdd6725a24e3c87cde24bbd38
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-06 15:05:53 +0600

    Set up a deploy

---
+++ aws-upload.conf.js
@@ -0,0 +1,6 @@
+module.exports = {
+  credentials: './aws-credentials.json',
+  bucketName: 'special-offer.widgets.thebookingfactory.com',
+  source: './dist',
+};
+
---
+++ scripts/deploy.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const mime = require('mime');
+const config = require('../aws-upload.conf.js');
+
+AWS.config.loadFromPath(config.credentials);
+
+const s3obj = new AWS.S3({ params: { Bucket: config.bucketName } });
+
+function upload(prefix) {
+  fs.readdirSync(`${config.source}/${prefix}`).forEach((filename) => {
+    if (fs.lstatSync(`${config.source}/${prefix}${filename}`).isDirectory()) {
+      upload(`${filename}/`);
+    } else {
+      const params = {
+        Body: fs.createReadStream(`${config.source}/${prefix}${filename}`),
+        Key: prefix + filename,
+        ContentType: mime.getType(`${config.source}/${prefix}${filename}`),
+      };
+
+      if (prefix === 'images/' || prefix === 'styles/') {
+        params.CacheControl = 'max-age=604800';
+      }
+
+      s3obj
+        .upload(params)
+        .on('httpUploadProgress', evt => console.log(evt))
+        .send((err, data) => console.log(err, data));
+    }
+  });
+}
+
+s3obj.listObjects({
+  Bucket: config.bucketName,
+}, (err, data) => {
+  if (data.Contents.length === 0) {
+    upload('');
+  } else {
+    s3obj.deleteObjects({
+      Bucket: config.bucketName,
+      Delete: {
+        Objects: data.Contents.map((el) => {
+          return { Key: el.Key };
+        }),
+      },
+    }, (error) => {
+      if (error == null) {
+        upload('');
+      }
+    });
+  }
+});
+
