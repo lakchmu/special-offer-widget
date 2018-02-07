abd2fbab5213dd32fa9be2448122a51c0702ae9c
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-07 19:13:11 +0600

    Edit event function

--- src/TBFSpecialOffers/offerEvents.js
+++ src/TBFSpecialOffers/offerEvents.js
@@ -1,3 +1,13 @@
 export default function offerElementClickEventHandler() {
-  this.classList.toggle('tbf-so-open');
+  const parentOffer = this.closest('.tbf-so-offer');
+  if (parentOffer) {
+    parentOffer.classList.toggle('tbf-so-open');
+    if (parentOffer.classList.contains('tbf-so-open')) {
+      this.textContent = 'Less';
+    } else {
+      this.textContent = 'More';
+    }
+  } else {
+    throw new Error('tbf-so-offer element not found');
+  }
 }
