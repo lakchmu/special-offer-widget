1998d290246599d73f460e48ed793770ee73b63f
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-08 14:31:01 +0600

    Refactoring getOffers test

--- src/__tests__/getOffers.test.js
+++ src/__tests__/getOffers.test.js
@@ -3,15 +3,19 @@ jest.mock('../TBFSpecialOffers/request');
 import getOffers from '../TBFSpecialOffers/getOffers';
 import OffersStorage from '../TBFSpecialOffers/offersStorage';
 
-const token = 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0';
-const filters = {};
+function setup() {
+  return { token: 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0', filters: {} };
+}
+
+test('Function getOffers return a promise', () => {
+  const setupData = setup();
+  expect(getOffers(setupData.token, setupData.filters)).toBeInstanceOf(Promise);
+});
 
 it('Function getOffers returns object of OffersStorage', () => {
+  const setupData = setup();
   expect.assertions(1);
-  return getOffers(token, filters)
+  return getOffers(setupData.token, setupData.filters)
     .then(offersStorage => expect(offersStorage).toBeInstanceOf(OffersStorage));
 });
 
-test('Function getOffers works with promise', () => {
-  expect(getOffers(token, filters)).toBeInstanceOf(Promise);
-});
