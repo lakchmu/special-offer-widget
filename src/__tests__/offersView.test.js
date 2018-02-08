1ca89f1b094663a6dfc7d3a123e726e1da5c70e1
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-08 21:52:22 +0600

    Refactoring offersView tests

--- src/__tests__/offersView.test.js
+++ src/__tests__/offersView.test.js
@@ -4,185 +4,140 @@ jest.mock('../index.css');
 import getOffers from '../TBFSpecialOffers/getOffers';
 import OffersView from '../TBFSpecialOffers/offersView';
 
-const token = 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0';
-const filters = {};
-const options = { defaultCSS: true, defaultEvents: true };
+function setup() {
+  const data = {
+    token: 'KO96pMbbIBTQPl7vnoJMXmIrEl2cYoUZS83QlL0Abu0',
+    filters: {},
+  };
+  return getOffers(data.token, data.filters);
+}
 
-it('Test OffersView class', () => {
-  expect.assertions(19);
-  // Set up our document body
-  document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      const offersView = new OffersView('.special-offers', offersStorage, options);
-      const root = document.querySelector('.special-offers');
-
-      // Test render function
-      // Test 'Offer' element
-      expect(root.querySelector('.special-offers .tbf-so-offer')).not.toBeNull();
-
-      // Test 'Header' element
-      expect(root.querySelector('.special-offers .tbf-so-offer__header')).not.toBeNull();
-
-      // Test 'Title' element
-      const title = root.querySelector('.special-offers .tbf-so-offer__title');
-      expect(title).not.toBeNull();
-      if (title) {
-        expect(title.textContent).toBe(offersStorage.list[0].title);
-      }
-
-      // Test 'Short description' element
-      const shortDescription = root.querySelector('.special-offers .tbf-so-offer__short-description');
-      expect(shortDescription).not.toBeNull();
-      if (shortDescription) {
-        expect(shortDescription.textContent).toBe(offersStorage.list[0].shortDescription);
-      }
-
-      // Test 'Content' element
-      expect(root.querySelector('.special-offers .tbf-so-offer__content')).not.toBeNull();
-
-      // Test 'Date from' and 'Date to' elements
-      const dates = root.querySelector('.special-offers .tbf-so-offer__dates');
-      expect(dates).not.toBeNull();
-      if (dates) {
-        expect(dates.textContent).toBe(`From ${offersStorage.list[0].dateFrom} to ${offersStorage.list[0].dateTo}`);
-      }
-
-      // Test 'Description' element
-      const description = root.querySelector('.special-offers .tbf-so-offer__description');
-      expect(description).not.toBeNull();
-      if (description) {
-        expect(description.textContent).toBe(offersStorage.list[0].description);
-      }
-
-      // Test 'Image' element
-      const image = root.querySelector('.special-offers .tbf-so-offer__image');
-      expect(image).not.toBeNull();
-      if (image) {
-        expect(image.src).toBe(offersStorage.list[0].imageLink);
-      }
-
-      // Test 'Discount' element
-      const discount = root.querySelector('.special-offers .tbf-so-offer__discount');
-      expect(discount).not.toBeNull();
-      if (discount) {
-        expect(discount.textContent).toBe(`${offersStorage.list[0].discountValue} ${offersStorage.list[0].discountType}`);
-      }
-
-      // Test 'BookingLink' element
-      const bookingLink = root.querySelector('.special-offers .tbf-so-offer__booking-link');
-      expect(bookingLink).not.toBeNull();
-      if (bookingLink) {
-        expect(bookingLink.href).toBe(offersStorage.list[0].bookingLink);
-      }
-
-      // Test events
-      const offer = root.querySelector('.tbf-so-offer');
-      offer.click();
-      expect(offer.classList.contains('tbf-so-open')).toBeTruthy();
-      offer.click();
-      expect(offer.classList.contains('tbf-so-open')).not.toBeTruthy();
-    });
-});
-
-it('Test using custom render function', () => {
-  expect.assertions(3);
-  // Set up our document body
-  document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      const offersView = new OffersView('.special-offers', offersStorage, options);
-      const root = document.querySelector('.special-offers');
-
-      options.renderTemplate = (offerModel) => {
-        const { title } = offerModel;
-
-        const template = `
-          <div class="tbf-so-offer">
-              <div class="tbf-so-offer__title">${title}</div>
-          </div>
-        `;
-        return template;
+function getOptions(key) {
+  let options;
+  switch (key) {
+    case 'empty':
+      options = {};
+      break;
+    case 'normal-render-function':
+      options = {
+        renderTemplate: (offerModel) => {
+          const { title } = offerModel;
+          const template = `<div class="tbf-so-offer">\n<div class="tbf-so-offer__title">${title}</div>\n</div>`;
+          return template;
+        },
       };
-
-      // Test custom render function
-      expect(root.querySelector('.special-offers .tbf-so-offer')).not.toBeNull();
-      const title = root.querySelector('.special-offers .tbf-so-offer__title');
-      expect(title).not.toBeNull();
-      if (title) {
-        expect(title.textContent).toBe(offersStorage.list[0].title);
-      }
-    });
-});
-
-it('Test otion.renderFunction is not function', () => {
-  expect.assertions(1);
-  // Set up our document body
-  document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      function wrapperNewOffersView() {
-        return new OffersView('.special-offers', offersStorage, options);
-      }
-      options.renderTemplate = 'not function';
-      expect(wrapperNewOffersView).toThrow();
-    });
-});
-
-it('Test otion.renderFunction return not string', () => {
-  expect.assertions(1);
-  // Set up our document body
-  document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      function wrapperNewOffersView() {
-        return new OffersView('.special-offers', offersStorage, options);
-      }
-      options.renderTemplate = (offerModel) => {
-        const { title } = offerModel;
-
-        const template = () => `
-          <div class="tbf-so-offer">
-              <div class="tbf-so-offer__title">${title}</div>
-          </div>
+      break;
+    case 'not function':
+      options = {
+        renderTemplate: 'not function',
+      };
+      break;
+    case 'render-function-return-not-a-string':
+      options = {
+        renderTemplate: (offerModel) => {
+          const { title } = offerModel;
+          return () => `
+            <div class="tbf-so-offer">
+                <div class="tbf-so-offer__title">${title}</div>
+            </div>
+            `;
+        },
+      };
+      break;
+    case 'render-function-without-offerModel-parameter':
+      options = {
+        renderTemplate: () => {
+          const title = 'Title';
+          const template = `
+            <div class="tbf-so-offer">
+                <div class="tbf-so-offer__title">${title}</div>
+            </div>
           `;
-        return template;
+          return template;
+        },
       };
-      expect(wrapperNewOffersView).toThrow();
+      break;
+    default:
+      break;
+  }
+  return options;
+}
+
+describe('Testing event listeners', () => {
+  it('Testing click event by "More" button', () => {
+    document.body.innerHTML = '<div class="special-offers"></div>';
+    expect.assertions(2);
+    return setup().then((offersStorage) => {
+      const offersView = new OffersView('.special-offers', offersStorage, getOptions('empty'));
+      const root = document.querySelector('.special-offers');
+      const moreButtons = root.querySelectorAll('.tbf-so-offer__more-link');
+      moreButtons.forEach((moreButton) => {
+        let parentOffer = moreButton.parentElement;
+        while (!parentOffer.classList.contains('tbf-so-offer') &&
+          !parentOffer.classList.contains('tbf-special-offers')) {
+          parentOffer = parentOffer.parentElement;
+        }
+        moreButton.click();
+        expect(parentOffer.classList.contains('tbf-so-open')).toBeTruthy();
+        moreButton.click();
+        expect(parentOffer.classList.contains('tbf-so-open')).not.toBeTruthy();
+      });
     });
+  });
 });
 
-it('Test. Root element does not exist', () => {
-  expect.assertions(1);
-  // Set up our document body
-  document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      function wrapperNewOffersView() {
-        return new OffersView('.not-existing-root-element', offersStorage, options);
-      }
-      expect(wrapperNewOffersView).toThrow();
+describe('Testing custom render function', () => {
+  it('Testing rendering', () => {
+    document.body.innerHTML = '<div class="special-offers"></div>';
+    const root = document.querySelector('.special-offers');
+    expect.assertions(1);
+    return setup().then((offersStorage) => {
+      const offersView = new OffersView('.special-offers', offersStorage, getOptions('normal-render-function'));
+      expect(root).toMatchSnapshot();
     });
+  });
+
+  it('Custom render function is not a function', () => {
+    document.body.innerHTML = '<div class="special-offers"></div>';
+    expect.assertions(1);
+    return setup().then((offersStorage) => {
+      function testWrapper() {
+        return new OffersView('.special-offers', offersStorage, getOptions('not function'));
+      }
+      expect(testWrapper).toThrow();
+    });
+  });
+
+  it('Custom render function return not a string', () => {
+    document.body.innerHTML = '<div class="special-offers"></div>';
+    expect.assertions(1);
+    return setup().then((offersStorage) => {
+      function testWrapper() {
+        return new OffersView('.special-offers', offersStorage, getOptions('render-function-return-not-a-string'));
+      }
+      expect(testWrapper).toThrow();
+    });
+  });
+
+  it('Custom render function without offerModel parameter', () => {
+    document.body.innerHTML = '<div class="special-offers"></div>';
+    expect.assertions(1);
+    return setup().then((offersStorage) => {
+      function testWrapper() {
+        return new OffersView('.special-offers', offersStorage, getOptions('render-function-without-offerModel-parameter'));
+      }
+      expect(testWrapper).toThrow();
+    });
+  });
 });
 
-it('Test using custom render function', () => {
-  expect.assertions(1);
-  // Set up our document body
+it('Root element for widget does not exist', () => {
   document.body.innerHTML = '<div class="special-offers"></div>';
-  return getOffers(token, filters)
-    .then((offersStorage) => {
-      function wrapperNewOffersView() {
-        return new OffersView('.special-offers', offersStorage, options);
-      }
-      options.renderTemplate = () => {
-        const title = 'Title';
-        const template = `
-          <div class="tbf-so-offer">
-              <div class="tbf-so-offer__title">${title}</div>
-          </div>
-        `;
-        return template;
-      };
-      expect(wrapperNewOffersView).toThrow();
-    });
+  expect.assertions(1);
+  return setup().then((offersStorage) => {
+    function testWrapper() {
+      return new OffersView('.not-existing-root-element', offersStorage, getOptions('empty'));
+    }
+    expect(testWrapper).toThrow();
+  });
 });
