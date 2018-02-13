c68046ad996ad19096c293eddf40359542eaff74
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-13 17:22:34 +0600

    Create test for response function

---
+++ src/__tests__/response.test.js
@@ -0,0 +1,8 @@
+import { response } from '../TBFSpecialOffers/request.js';
+
+test('Test response function. It complete with throw if server answer with error', () => {
+  function testWrapper() {
+    return response({ ok: false, status: 404 });
+  }
+  expect(testWrapper).toThrow();
+});
