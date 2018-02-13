62710c79a6194b4be429fba2d3d820ea0ad35037
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-02-13 20:30:52 +0600

    Remove unused check from GetOffers class

--- src/TBFSpecialOffers/getOffers.js
+++ src/TBFSpecialOffers/getOffers.js
@@ -6,12 +6,8 @@ function getOffers(token, filters) {
   const url = API_END_POINT + API_METHOD_SPECIAL_OFFERS;
   return request(url, 'get', token)
     .then(response => response.json())
-    .then((json) => {
-      if (json.errors) {
-        throw new Error(json.errors.reduce((previousError, currentError) => `${previousError}, ${currentError}`));
-      }
-      return new OffersStorage(json.special_offers);
-    }).catch((error) => {
+    .then(json => new OffersStorage(json.special_offers))
+    .catch((error) => {
       throw new Error(error.message);
     });
 }
