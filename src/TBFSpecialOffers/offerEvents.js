07589899f6b4f3202b8f1fa3938068396c200b1e
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-03-30 13:54:46 +0600

    Refactoring "expandSpecialOfferDescription" function

--- src/TBFSpecialOffers/offerEvents.js
+++ src/TBFSpecialOffers/offerEvents.js
@@ -1,17 +1,24 @@
+function checkParentElement(parentElement) {
+  return parentElement.classList.contains('tbf-so-offer') ||
+    parentElement.classList.contains('tbf-special-offers');
+}
+
+function getNewText(parentOffer) {
+  return (parentOffer.classList.contains('tbf-so-open')) ?
+    'Less' :
+    'More';
+}
+
 export default function expandSpecialOfferDescription() {
   let parentOffer = this.parentElement;
-  while (!parentOffer.classList.contains('tbf-so-offer') &&
-         !parentOffer.classList.contains('tbf-special-offers')) {
+  while (!checkParentElement(parentOffer)) {
     parentOffer = parentOffer.parentElement;
   }
   if (parentOffer.classList.contains('tbf-so-offer')) {
     parentOffer.classList.toggle('tbf-so-open');
-    if (parentOffer.classList.contains('tbf-so-open')) {
-      this.textContent = 'Less';
-    } else {
-      this.textContent = 'More';
-    }
+    this.textContent = getNewText(parentOffer);
   } else {
     throw new Error('tbf-so-offer element not found');
   }
 }
+
