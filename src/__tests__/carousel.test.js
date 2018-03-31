002cea674057314558b84795e0f067f71da15617
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-03-31 19:01:42 +0600

    Refactoring carousel.test

--- src/__tests__/carousel.test.js
+++ src/__tests__/carousel.test.js
@@ -13,51 +13,47 @@ function setup() {
   return getOffers(data.token);
 }
 
+function setupTest(rootElement) {
+  document.body.innerHTML = '<div class="special-offers"></div>';
+  const options = { view: 'carousel' };
+  return setup()
+    .then(offersStorage => new OffersView(rootElement, offersStorage, options));
+}
+
 describe('Testing Carousel class', () => {
   it('Testing render function', () => {
-    document.body.innerHTML = '<div class="special-offers"></div>';
-    const root = document.querySelector('.special-offers');
-    const options = { view: 'carousel' };
     expect.assertions(1);
-    return setup().then((offersStorage) => {
-      const offersView = new OffersView('.special-offers', offersStorage, options);
-      expect(root).toMatchSnapshot();
-    });
+    return setupTest('.special-offers')
+      .then(offersView => expect(offersView.rootElement).toMatchSnapshot());
   });
 
   it('Testing arrow event listener', () => {
-    document.body.innerHTML = '<div class="special-offers"></div>';
-    const root = document.querySelector('.special-offers');
-    const options = { view: 'carousel' };
     expect.assertions(4);
-    return setup().then((offersStorage) => {
-      const offersView = new OffersView('.special-offers', offersStorage, options);
-      const leftArrow = document.querySelector('.tbf-so-arrow[data-direction="to-left"]');
-      const rightArrow = document.querySelector('.tbf-so-arrow[data-direction="to-right"]');
-      const indicators = document.querySelectorAll('.tbf-so-carousel-indicator');
-      rightArrow.click();
-      expect(document.querySelectorAll('.tbf-so-carousel-indicator.active').length).toBe(1);
-      expect(indicators[1].classList.contains('active')).toBeTruthy();
-      leftArrow.click();
-      expect(document.querySelectorAll('.tbf-so-carousel-indicator.active').length).toBe(1);
-      expect(indicators[0].classList.contains('active')).toBeTruthy();
-    });
+    return setupTest('.special-offers')
+      .then(() => {
+        const leftArrow = document.querySelector('.tbf-so-arrow[data-direction="to-left"]');
+        const rightArrow = document.querySelector('.tbf-so-arrow[data-direction="to-right"]');
+        const indicators = document.querySelectorAll('.tbf-so-carousel-indicator');
+        rightArrow.click();
+        expect(document.querySelectorAll('.tbf-so-carousel-indicator.active')).toHaveLength(1);
+        expect(indicators[1].classList.contains('active')).toBeTruthy();
+        leftArrow.click();
+        expect(document.querySelectorAll('.tbf-so-carousel-indicator.active')).toHaveLength(1);
+        expect(indicators[0].classList.contains('active')).toBeTruthy();
+      });
   });
 
   it('Testing indicator event listener', () => {
-    document.body.innerHTML = '<div class="special-offers"></div>';
-    const root = document.querySelector('.special-offers');
-    const options = { view: 'carousel' };
     expect.assertions(4);
-    return setup().then((offersStorage) => {
-      const offersView = new OffersView('.special-offers', offersStorage, options);
-      const indicators = document.querySelectorAll('.tbf-so-carousel-indicator');
-      indicators[1].click();
-      expect(indicators[1].classList.contains('active')).toBeTruthy();
-      expect(document.querySelectorAll('.tbf-so-carousel-indicator.active').length).toBe(1);
-      indicators[0].click();
-      expect(indicators[0].classList.contains('active')).toBeTruthy();
-      expect(document.querySelectorAll('.tbf-so-carousel-indicator.active').length).toBe(1);
-    });
+    return setupTest('.special-offers')
+      .then(() => {
+        const indicators = document.querySelectorAll('.tbf-so-carousel-indicator');
+        indicators[1].click();
+        expect(indicators[1].classList.contains('active')).toBeTruthy();
+        expect(document.querySelectorAll('.tbf-so-carousel-indicator.active')).toHaveLength(1);
+        indicators[0].click();
+        expect(indicators[0].classList.contains('active')).toBeTruthy();
+        expect(document.querySelectorAll('.tbf-so-carousel-indicator.active')).toHaveLength(1);
+      });
   });
 });
