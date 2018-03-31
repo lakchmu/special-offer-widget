1977316207906624e4ce656179721361a7e802d8
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-03-31 18:31:35 +0600

    Fix typo in import in response.test

--- src/__tests__/response.test.js
+++ src/__tests__/response.test.js
@@ -1,6 +1,6 @@
 /* global test, expect */
 
-import { getResponse } from '../TBFSpecialOffers/request.js';
+import { getResponse } from '../TBFSpecialOffers/request';
 
 test('Test response function. It complete with throw if server answer with error', () => {
   function testWrapper() {
