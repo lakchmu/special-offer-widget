09c7d774957f5dfa41cad64e81ea4106e451c589
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-03-31 18:30:34 +0600

    Rename response function to getResponse

--- src/__tests__/response.test.js
+++ src/__tests__/response.test.js
@@ -1,10 +1,10 @@
 /* global test, expect */
 
-import { response } from '../TBFSpecialOffers/request.js';
+import { getResponse } from '../TBFSpecialOffers/request.js';
 
 test('Test response function. It complete with throw if server answer with error', () => {
   function testWrapper() {
-    return response({ ok: false, status: 404 });
+    return getResponse({ ok: false, status: 404 });
   }
   expect(testWrapper).toThrow();
 });
