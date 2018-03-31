6473017ad272b62dedff3e7ee025f13771fe539b
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-03-31 19:14:43 +0600

    Refactoring OfferModel class

--- src/TBFSpecialOffers/offerModel.js
+++ src/TBFSpecialOffers/offerModel.js
@@ -1,26 +1,14 @@
 class OfferModel {
   constructor(offer) {
-    const {
-      title,
-      short_description,
-      description,
-      date_from,
-      date_to,
-      discount_value,
-      discount_type,
-      link,
-      image,
-    } = offer;
-
-    this.title = title;
-    this.shortDescription = short_description;
-    this.description = description;
-    this.dateFrom = new Date(date_from);
-    this.dateTo = new Date(date_to);
-    this.imageLink = image;
-    this.discountValue = discount_value;
-    this.discountType = discount_type;
-    this.bookingLink = link;
+    this.title = offer.title;
+    this.shortDescription = offer.short_description;
+    this.description = offer.description;
+    this.dateFrom = new Date(offer.date_from);
+    this.dateTo = new Date(offer.date_to);
+    this.imageLink = offer.image;
+    this.discountValue = offer.discount_value;
+    this.discountType = offer.discount_type;
+    this.bookingLink = offer.link;
   }
 }
 
