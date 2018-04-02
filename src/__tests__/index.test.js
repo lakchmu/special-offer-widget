1ef7060c766693a0106deae298f6d7d94352c245
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-04-02 15:15:57 +0600

    Add test for catch branch of TBFSpecialOffer function

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,9 +1,10 @@
-/* global jest, it, expect */
+/* global jest, describe, it, expect */
 
 import TBFSpecialOffer from '../index';
 
 jest.mock('../TBFSpecialOffers/request');
 jest.mock('../index.css');
+console.warn = jest.fn();
 
 function setup() {
   return {
@@ -18,4 +19,13 @@ it('Test TBFSpecialOffer function with default settings', () => {
   expect.assertions(1);
   return TBFSpecialOffer(setupData.targetDivElement, setupData.token)
     .then(offersView => expect(offersView).toMatchSnapshot());
+});
+
+it('Test TBFSpecialOffer function with wrong token', () => {
+  document.body.innerHTML = '<div class="special-offers"></div>';
+  const setupData = setup();
+  expect.assertions(1);
+  return TBFSpecialOffer(setupData.targetDivElement, '')
+    .then(() =>
+      expect(console.warn.mock.calls).toHaveLength(1));
 });
