b1166fcbde4ffdb59a53f4f87303aac49dcb6c7c
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-04-02 15:17:49 +0600

    Add test for request function

--- src/__tests__/response.test.js
+++ src/__tests__/response.test.js
@@ -1,10 +1,26 @@
-/* global test, expect */
+/* global jest, test, expect */
 
-import { getResponse } from '../TBFSpecialOffers/request';
+import request, { getResponse } from '../TBFSpecialOffers/request';
+
+fetch = jest.fn(); // eslint-disable-line no-global-assign
+
+fetch
+  .mockReturnValue(new Promise(resolve => resolve({ ok: true })))
+
+function setup() {
+  return { url: 'https://url', method: 'get', tokenValue: 'token' };
+}
 
 test('Test response function. It complete with throw if server answer with error', () => {
   function testWrapper() {
     return getResponse({ ok: false, status: 404 });
   }
   expect(testWrapper).toThrow();
+});
+
+it('Test request function', () => {
+  const data = setup();
+  expect.assertions(1);
+  return request(data.url, data.method, data.tokenValue)
+    .then(() => expect(fetch.mock.calls).toHaveLength(1));
 });
