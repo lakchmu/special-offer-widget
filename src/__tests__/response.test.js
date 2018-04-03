a3c586fb4f77d32785df8f72590eab39f7f23656
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-04-03 10:53:54 +0600

    Add test for request function

--- src/__tests__/response.test.js
+++ src/__tests__/response.test.js
@@ -4,9 +4,6 @@ import request, { getResponse } from '../TBFSpecialOffers/request';
 
 fetch = jest.fn(); // eslint-disable-line no-global-assign
 
-fetch
-  .mockReturnValue(new Promise(resolve => resolve({ ok: true })))
-
 function setup() {
   return { url: 'https://url', method: 'get', tokenValue: 'token' };
 }
@@ -21,6 +18,19 @@ test('Test response function. It complete with throw if server answer with error
 it('Test request function', () => {
   const data = setup();
   expect.assertions(1);
+  fetch
+    .mockReturnValue(new Promise(resolve => resolve({ ok: true })));
   return request(data.url, data.method, data.tokenValue)
     .then(() => expect(fetch.mock.calls).toHaveLength(1));
+});
+
+it('Test request function2', () => {
+  const data = setup();
+  expect.assertions(1);
+  fetch
+    .mockReturnValue(new Promise((resolve, reject) => reject(new Error('Error'))));
+  function testWrapper() {
+    return request(data.url, data.method, data.tokenValue);
+  }
+  return expect(testWrapper()).rejects.toThrow();
 });
